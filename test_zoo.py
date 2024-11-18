bfe6a0ea41e5d91eb71bd3059e5321698deb96af
Aunyada_K <151939052+aunyada05@users.noreply.github.com>
2024-11-18 10:29:40 +0000

    Added unit test cases

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,20 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "This is not a valid age")
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if 0 <= age <= 12: # Fix:  0 < to 0 <=
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: # Fix: < 20 to <= 20
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: # Fix: 21 < to 21 <=
             return 150
-        elif age >= 60:
+        elif age > 60: # Fix: >= 60 to > 60
             return 100
+        else:
+            return "This is not a valid age"
